d9d7545be8b3b73260bf30ee06a842b5a2d6968f
itphilip1998 <itkeyzzpianny@gmail.com>
2022-03-03 14:16:24 +0100

    dev

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 require("./config/database")();
 const web = require("./routes/web");
-const api = require("./routes/api");
+// const api = require("./routes/api");
 const channel = require("./routes/channel");
 const app = express();
 const cors = require("cors");
@@ -11,19 +11,17 @@ const port = process.env.PORT || 8000;
 // use
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-// app.use(express.static("public"));
-// app.set("views", "views");
-// app.set("view engine", "hbs");
+app.use(express.static("public")); 
 app.use(cors());
 
 // routes
 app.use("/", web);
-app.use("/api", api);
+// app.use("/api", api);
 app.use("/channel", channel);
 
 // Express Error Handling
 app.get("*", (req, res, next) => {
-  res.render("pageNotFound");
+  res.send("Page Not Found!");
   next();
 });
 
