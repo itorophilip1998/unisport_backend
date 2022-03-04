42840a5747d4f54c8d1733b61b251737954ee558
Itoro Emmanuel Philip <56868676+itorophilip1998@users.noreply.github.com>
2022-03-04 15:56:24 +0100

    Test
    Test

--- index.js
+++ index.js
@@ -12,7 +12,13 @@ const port = process.env.PORT || 8000;
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public")); 
-app.use(cors());
+var corsOptions = {
+    origin: "*",
+    optionsSuccessStatus: 200 // For legacy browser support
+    methods: "GET, PUT,POST,DELETE"
+}
+
+app.use(cors(corsOptions));
 
 // routes
 app.use("/", web);
