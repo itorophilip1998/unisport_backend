9e3aba78e9a7739fcac4926408e9f27d8c819991
Itoro Emmanuel Philip <56868676+itorophilip1998@users.noreply.github.com>
2022-03-04 16:50:39 +0100

    test
    test

--- index.js
+++ index.js
@@ -13,10 +13,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public")); 
 
-const corsOptions = {
-	origin: "http://localhost:3000"
-};
-app.use(cors(corsOptions));
+ 
+app.use(cors());
 
 
 // routes
