6b23124fba0c80f8c6eef9332f6f13755add1b0f
Itoro Emmanuel Philip <56868676+itorophilip1998@users.noreply.github.com>
2022-03-04 16:40:25 +0100

    test
    test

--- index.js
+++ index.js
@@ -12,7 +12,13 @@ const port = process.env.PORT || 8000;
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public")); 
-app.options('*', cors()) 
+const option={
+  "origin": "*",
+  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+  "preflightContinue": false,
+  "optionsSuccessStatus": 204
+};
+app.use(cors(option));
 
 // routes
 app.use("/", web);
